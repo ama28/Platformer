eb3d79e237ed7ede20ac9a68247fd57d940ee760
YouDoctorYet <zun5401@gmail.com>
2022-03-18 01:22:33 -0400

    projectile enemy

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float fire_speed = 20f; 
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * fire_speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Destroy(other.gameObject);
+            Destroy(gameObject);
+            RestartScene();
+        }
+    }
+
+    void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/ProjectileEnemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileEnemy : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+
+    public float fireRate;
+    float nextFire;
+    void Start()
+    {
+        nextFire = Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTimeOver();
+    }
+
+    void CheckTimeOver()
+    {
+        if(Time.time > nextFire)
+        {
+            Instantiate(bullet, new Vector2(transform.position.x + .5f, transform.position.y) , transform.rotation);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
