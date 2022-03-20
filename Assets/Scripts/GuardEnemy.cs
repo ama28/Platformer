1e23038c8ff2edb532715e195909f6da88100575
YouDoctorYet <zun5401@gmail.com>
2022-03-20 17:23:21 -0400

    Delayed enemy turn

--- Assets/Scripts/GuardEnemy.cs
+++ Assets/Scripts/GuardEnemy.cs
@@ -8,25 +8,38 @@ public class GuardEnemy : MonoBehaviour
     Transform target;
     private Animator myAnim;
     public float delay = 2f;
+    float turnTime = 2f; 
+    Vector2 currentTarget;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         myAnim = GetComponent<Animator>();
+        currentTarget = new Vector2(target.position.x, transform.position.y);
+    }
+
+    void TurnTowardPlayer()
+    {
+        if(turnTime <= 0){
+            if (target.position.x > transform.position.x)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                currentTarget = new Vector2(target.position.x, transform.position.y);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                currentTarget = new Vector2(target.position.x, transform.position.y);
+            }
+            turnTime = 2f;
+        }
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+        turnTime -= Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
         myAnim.SetFloat("GuardSpeed", Mathf.Abs(target.position.x - transform.position.x));
-
-        if (target.position.x > transform.position.x)
-        {
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-        }
-        else
-        {
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
+        TurnTowardPlayer();
     }
 }
